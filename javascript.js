3aec6115385ce668f73705dca145668cf4f726cf
Iana Pavlycheva <ianapavlycheva@gmail.com>
2024-08-21 12:30:02 +1000

    Set up a hover effect

--- javascript.js
+++ javascript.js
@@ -3,5 +3,10 @@ const container = document.getElementById("gridContainer");
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.classList.add("grid-square");
+
+  div.addEventListener("mouseover", function () {
+    div.style.backgroundColor = "black";
+  });
+
   container.appendChild(div);
 }
